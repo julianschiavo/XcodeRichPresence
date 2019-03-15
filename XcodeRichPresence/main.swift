2209ee7e010ec784e45d9cca4c03af720ee9b53b
Julian <justdotjs@icloud.com>
2019-03-15 21:04:40 +0800

    Initial Commit

---
+++ XcodeRichPresence/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  XcodeRichPresence
+//
+//  Created by Julian Schiavo on 15/3/2019.
+//  Copyright © 2019 Julian Schiavo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
